86fdb0f93e2ee6b94e46beb4ce9602f126ab1e0b
Utkarsh Singh <utk608@gmail.com>
2024-12-29 23:17:11 +0530

    Added RestTemplate Call in the UserService

--- UserService/src/main/java/com/utkarsh/UserService/service/UserService.java
+++ UserService/src/main/java/com/utkarsh/UserService/service/UserService.java
@@ -1,22 +1,31 @@
 package com.utkarsh.UserService.service;
 
+import com.utkarsh.UserService.entity.Rating;
 import com.utkarsh.UserService.entity.User;
 import com.utkarsh.UserService.exception.ResourceNotFoundException;
 import com.utkarsh.UserService.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @Service
 public class UserService implements IUserService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserService.class);
     private static final String NO_USER_FOUND_FOR_ID = "No user found for id: ";
+    private static final String USER_RATING_ENDPOINT = "http://localhost:8083/ratings/users/";
     private final UserRepository userRepository;
+    private final RestTemplate restTemplate;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, RestTemplate restTemplate) {
         this.userRepository = userRepository;
+        this.restTemplate = restTemplate;
     }
 
     @Override
@@ -33,7 +42,12 @@ public class UserService implements IUserService {
 
     @Override
     public User getUserById(String userId) {
-        return userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException(NO_USER_FOUND_FOR_ID + userId));
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException(NO_USER_FOUND_FOR_ID + userId));
+        //Fetch user rating from the Rating Service
+        //ratings/users/{userId}
+        List<Rating> response = restTemplate.getForObject(USER_RATING_ENDPOINT + userId, ArrayList.class);
+        user.setRatings(response);
+        return user;
     }
 
     @Override
